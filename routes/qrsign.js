aea486b81e3a602adb3c324469536a14c883ea85
baoanj <baoanj@live.com>
2017-09-26 23:21:02 +0800

    修复bug,优化签到记录页面UI

--- routes/qrsign.js
+++ routes/qrsign.js
@@ -7,6 +7,25 @@ var qr = require('qr-image');
 module.exports = function(db) {
 	var signManager = require('../model/signModel')(db);
 
+	router.get('/sign', function(req, res, next) {
+		res.render('qrsign', { id: req.query.id, date: req.query.date, title: req.query.classmsg });
+	});
+
+	router.post('/sign', function(req, res, next) {
+		var username = req.query.id;
+		var date = formatDate(new Date(+req.query.date));
+		signManager.insertSignRecord(username, date, req.body.signmsg)
+		res.send('签到成功');
+	});
+
+	router.all('*', function(req, res, next) {
+		if (req.session.user) {
+			next();
+		} else {
+			res.redirect('/');
+		}
+	});
+
 	router.get('/', function(req, res, next) {
 		var id = req.session.user.username;
 		var date = Date.now();
@@ -15,25 +34,15 @@ module.exports = function(db) {
 		res.send(svg_string);
 	});
 
-	router.get('/sign', function(req, res, next) {
-		res.render('qrsign', { id: req.query.id, date: req.query.date, classmsg: req.query.classmsg });
-	});
-
 	router.get('/record', function(req, res, next) {
 		signManager.findAllSignRecords(req.session.user.username).then(function(result) {
-			var records = [];
+			var records = {};
 			result.forEach(function(val) {
-				records.push(val.content);
+				if (!records[val.date]) records[val.date] = [];
+				records[val.date].push(val.content);
 			});
-			res.render('signrecord', { records: records });
+			res.render('signrecord', { exist: true, records: sortRecordsByDate(records), title: '签到记录' });
 		});
-	});
-
-	router.post('/sign', function(req, res, next) {
-		var username = req.query.id;
-		var date = formatDate(new Date(+req.query.date));
-		signManager.insertSignRecord(username, date, req.body.signmsg)
-		res.send('签到成功');
 	});
 
 	return router;
@@ -47,4 +56,14 @@ function formatDate(date) {
 	var min = date.getMinutes();
 	var sec = date.getSeconds();
 	return '' + year + '年' + month + '月' + day + '日 ' + hour + ':' + min + ':' + sec;
+}
+
+function sortRecordsByDate(records) {
+	var rcds = {};
+	var keys = Object.keys(records);
+	keys.reverse();
+	keys.forEach(function(key) {
+		rcds[key] = records[key];
+	});
+	return rcds;
 }
